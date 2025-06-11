c141e644c5aa7fabb77f94762e2e112bf13a5644
habarcs <mhevizi@gmail.com>
2025-06-11 09:08:30 +0200

    comment

--- models/smp_model.py
+++ models/smp_model.py
@@ -53,7 +53,7 @@ class SmpModel(torch.nn.Module):
         x = torch.cat((x_t, lq), dim=1)
         self.check_input_shape(x)
 
-        t_embedded = self.t_embedding(t - 1)
+        t_embedded = self.t_embedding(t - 1) # timesteps are between 1 - T, but for embedding need to be 0 - T-1
 
         features = self.encoder(x)
         time_embedded_features = [
