5fab8194f177733061a9bf2a1f9b7b986a685966
habarcs <mhevizi@gmail.com>
2025-11-25 15:31:34 +0100

    add scripts to change config and run training

---
+++ scripts/diffusion_with_reduced_t.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P)
+
+
+config_file=$(dirname "$dir")/configs/diffusion_with_reduced_t.py
+target=$(dirname "$dir")/config.py
+train_file=$(dirname "$dir")/train.py
+
+echo Copying "$config_file" to "$target"
+cp "$config_file" "$target"
+
+echo Starting training
+python "$train_file"
